7b06129e13c6eeb5ef46e5250fe328b61c206005
blackpirate <mr.blackpirate@yandex.ru>
2019-06-14 12:43:39 +0300

    water fix, changes in level

--- Platformer/Assets/Scripts/CharacterController.cs
+++ Platformer/Assets/Scripts/CharacterController.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float Speed;
+    public float FlyingSpeed;
     public float JumpForce;
     public float MaxJumpDistance;
     public float RotationSpeed;
+    public float JumpHeight;
 
     public float DeathHeight;
     public Transform StartPoition;
 
     private Rigidbody rigidbody;
     private RaycastHit hit;
+    private float currentSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody>();
+        currentSpeed = Speed;
     }
 
     // Update is called once per frame
@@ -31,23 +35,32 @@ public class CharacterController : MonoBehaviour
         }
         else
         {
-            var deltaZ = -Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
-            var deltaX = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-            var mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
             bool isJump = false;
 
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Physics.Raycast(gameObject.transform.position, Vector3.down, out hit) && hit.distance <= MaxJumpDistance)
             {
-                if (Physics.Raycast(gameObject.transform.position, Vector3.down, out hit))
+                currentSpeed = Speed;
+                if (Input.GetKeyDown(KeyCode.Space))
                 {
-                    Debug.Log("hit " + hit.collider);
-                    if (hit.distance <= MaxJumpDistance)
-                        isJump = true;
+                    isJump = true;
+                    currentSpeed = FlyingSpeed;
                 }
             }
+            //Debug.Log(currentSpeed);
+
+            var deltaZ = -Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
+            var deltaX = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
+            var mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
 
             gameObject.transform.Rotate(0, mouseX, 0);
-            rigidbody.AddForce(0, isJump ? JumpForce : 0, 0);
+            if (isJump && Input.GetKey(KeyCode.W))
+            {
+                rigidbody.AddForce(transform.right.x * JumpForce, JumpForce * JumpHeight, transform.right.z * JumpForce);
+            }
+            else if (isJump)
+            {
+                rigidbody.AddForce(0, JumpForce * JumpHeight, 0);
+            }
             gameObject.transform.Translate(deltaX, 0, deltaZ);
         }
     }
