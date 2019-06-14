6944f4340494b554c46bacaa1b96d12c5b03565a
Соловьева Эвелина Сергеевна <evssoloveva@stud.kpfu.ru>
2019-06-14 12:49:04 +0300

    hollow

---
+++ Platformer/Assets/Scripts/HollowMoving.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HollowMoving : MonoBehaviour
+{
+    public float LenghtMove;
+    public float WeightMove;
+
+    private Vector3 startPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint = gameObject.transform.position;
+        Debug.Log(startPoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(gameObject.transform.position.z - startPoint.z) < LenghtMove)
+        {
+            gameObject.transform.Translate(Vector3.forward*Time.deltaTime);
+            Debug.Log("forward");
+        }
+        else
+        {
+            if (Mathf.Abs(gameObject.transform.position.x - startPoint.x) < WeightMove)
+            {
+                gameObject.transform.Translate(Vector3.right * Time.deltaTime);
+                Debug.Log("right");
+            }
+            else
+            {
+                if (Mathf.Abs(gameObject.transform.position.z - startPoint.z) >= LenghtMove)
+                {
+                    gameObject.transform.Translate(Vector3.back * Time.deltaTime);
+                    Debug.Log("back");
+                }
+                else
+                {
+                    if (Mathf.Abs(gameObject.transform.position.x - startPoint.x) >= WeightMove)
+                    {
+                        gameObject.transform.Translate(Vector3.left * Time.deltaTime);
+                        Debug.Log("left");
+                    }
+                }
+            }
+        }
+    }
+}
