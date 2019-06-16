edc89cbd4aa21549ef614f1e9385bd2135bb5e2e
blackpirate <mr.blackpirate@yandex.ru>
2019-06-16 14:01:14 +0300

    moving camera

---
+++ Platformer/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float MaxUpAngle;
+    public float MaxDownAngle;
+    public float Sensivity;
+
+    private float rotation;
+    private Vector3 baseRotation;
+
+    void Start()
+    {
+        baseRotation = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation -= Input.GetAxis("Mouse Y") * Sensivity;
+        rotation = Mathf.Clamp(rotation, MaxDownAngle, MaxUpAngle);
+        transform.localEulerAngles = new Vector3(baseRotation.x, baseRotation.y, baseRotation.z + rotation);
+    }
+
+}
